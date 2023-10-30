974ed615fa7578b81548e2c90e1031a73a3861fa
Pirisinu <palazzialessandro2@gmail.com>
2023-10-30 15:36:33 +0100

    [ADD]Fake array in js for test 'v-for' in html

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const { createApp } = Vue ;
 createApp({
   data(){
     return{
-      title: 'Ciao prova'
+      title: 'Todo-List',
+      fintoArray:[
+        "Fare una corsa",
+        "Incontrare gli amici",
+        "Andare in montagna",
+        "Chiamare l'idraulico",
+        "Pitturare casa"
+      ]
     }
   }
 }).mount('#app');
