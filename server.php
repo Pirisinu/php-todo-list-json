67b3d3d8ec09f17d73a1018e92200b6027155c40
Pirisinu <palazzialessandro2@gmail.com>
2023-11-06 10:22:51 +0100

    [FIX]json_decode

--- server.php
+++ server.php
@@ -3,10 +3,10 @@
 $json_todo_string = file_get_contents('todo-list.json');
 
 //Trasformo la stringa in array PHP
-$list_todo = json_decode($json_todo_string);
+$list_todo = json_decode($json_todo_string, true);
 
-/*logica per ricodifica array json*/
-
+  /*Logica Per Ricodifica array json*/
+/* NEW TASK */
 if(isset($_POST['newTaskItem'])){
   $newTask = [
     "task" => $_POST['newTaskItem'],
@@ -16,18 +16,20 @@ if(isset($_POST['newTaskItem'])){
   $list_todo[] = $newTask;
   file_put_contents('todo-list.json', json_encode($list_todo));
 }
+/* TASK TOGGLE DONE */
 if (isset($_POST['indexToggle'])) {
   $index = $_POST['indexToggle'];
-  $list_todo[$index]->done = !$list_todo[$index]->done;
+  $list_todo[$index]["done"] = !$list_todo[$index]["done"] ;
   file_put_contents('todo-list.json', json_encode($list_todo));
 }
+/* TASK DELETE */
 if (isset($_POST['indexDel'])) {
   $index = $_POST['indexDel'];
-  $newarray = array_splice($list_todo, $index, 1);
-  file_put_contents('todo-list.json', json_encode($list_todo));
+  if ($list_todo[$index]["done"]) {
+    array_splice($list_todo, $index, 1);
+    file_put_contents('todo-list.json', json_encode($list_todo));
+  }
 }
-
-
 
 //Trasformo il file PHP in un file Json
 header('Content-Type: application/json');
